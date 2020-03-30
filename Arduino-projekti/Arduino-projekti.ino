a4d8abc9c9e8fc7a0511f21ab248344f61c17166
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-03-30 21:27:45 +0300

    Add tone when water level drop

--- Arduino-projekti/Arduino-projekti.ino
+++ Arduino-projekti/Arduino-projekti.ino
@@ -1,18 +1,20 @@
 int waterPumpPin = 1;
+int waterLevelPin = A0;
+int humidityPin = A1;
 
 // Get water level
 int getWaterLevel() {
-  int sensorValue = analogRead(A0)
+  int sensorValue = analogRead(waterLevelPin);
   return sensorValue;
 }
 
 // Get flower pot humidity
 int getHumidity() {
-  int sensorValue = analogRead(A0)
+  int sensorValue = analogRead(humidityPin);
   return sensorValue;
 }
 
-// 
+//
 void startWaterPump() {
   // Wait 12h before starting water pump
   delay(4320000);
@@ -27,15 +29,17 @@ void startWaterPump() {
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  
+  pinMode(13, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-    if(getHumidity() < 100) {
-    startWaterPump();
+  Serial.println(getWaterLevel());
+  if (getWaterLevel() < 200) {
+    tone(13, 1000, 200);
   }
-
+  if (getHumidity() < 200) {
+    startWaterPump();
   }
   delay(1);        // delay in between reads for stability
 }
